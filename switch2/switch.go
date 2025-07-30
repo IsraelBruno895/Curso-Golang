9275c3fed22f7728fc0f129217b35456b5238cd3
Israel <israel.franca895@outlook.com>
2025-07-30 11:18:02 -0300

    Adicionando switch2

---
+++ switch2/switch.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+	switch { // swict true
+	case t.Hour() < 12:
+		fmt.Println("BOM DIA!")
+	case t.Hour() < 18:
+		fmt.Println("BOA TARDE")
+	default:
+		fmt.Println("BOA NOITE")
+	}
+}
